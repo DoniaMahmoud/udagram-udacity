9f02d1684ec75640621ee84162b3d8a3be013580
DoniaMahmoud <donia1999mahmoud@gmail.com>
2022-10-18 22:13:04 +0200

    linting

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://udacityapp-env.eba-w3p2uaze.us-east-1.elasticbeanstalk.com/api/v0",
+  appName: 'Udagram',
+  apiHost: 'http://udacityapp-env.eba-w3p2uaze.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
